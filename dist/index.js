d511b05cb73c3ac2f67398e20db72fc8bc85a03e
YPZ-24 <aylinypz@gmail.com>
2020-09-14 14:55:57 -0500

    YYY

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
 "use strict";
-/*import {config} from 'dotenv';
-config();*/
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+dotenv_1.config();
 const database_1 = __importDefault(require("./database"));
 database_1.default();
 const app_1 = __importDefault(require("./app"));
